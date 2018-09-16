1865a68ce7254cbabc66179cd6e7924abe67c71d
squall1744 <squall1744@163.com>
2018-09-16 14:56:35 +0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Header from './common/header/Header'
 import store from './store/store'
 import {Provider} from 'react-redux'
+import {BrowserRouter, Route} from 'react-router-dom' 
 
-export const App = (
-  <Provider store={store}>
-    <Header></Header>
-  </Provider>
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Fragment>
+          <Header />      
+          <BrowserRouter>
+            <Fragment>
+              <Route path="/" exact render={() => <div>ddd</div>}></Route>
+              <Route path="/aaa" exact render={() => <div>xxx</div>}></Route>
+            </Fragment>
+          </BrowserRouter>
+        </Fragment>
+      </Provider>
+    )
+  }
+}
 
-)
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {App} from './App'
+import App from './App'
 import './style.js'
 import './static/iconfont/iconfont.js'
 
-ReactDOM.render(App, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
