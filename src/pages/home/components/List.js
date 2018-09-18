f8c34044267b64bde7636fc8e693a3fe63da0ebd
squall1744 <squall1744@163.com>
2018-09-18 16:30:12 +0800

    add List component

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,9 +1,47 @@
 import React, {Component} from 'react'
+import {ListWrapper, ListItem, ListInfo} from '../style' 
 
 class List extends Component {
   render() {
     return (
-      <div>List</div>
+      <ListWrapper>
+        <ListItem>
+          <ListInfo>
+            <h3 className="title">Chrome吃内存的能力可不是说着玩的!</h3>
+            <p className="desc">坏新闻总比没新闻要好！前段时间, 经过一波朋友圈的疯狂转发,大家都在讨论"红芯浏览器", 不管是不是套路, 反正是出名了...最后的结论就是, ...</p>
+          </ListInfo>
+          <img className="list-pic" src ="https://upload-images.jianshu.io/upload_images/3203841-bd64bf1ec52425b9.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240" />
+        </ListItem>
+        <ListItem>
+          <ListInfo>
+            <h3 className="title">Chrome吃内存的能力可不是说着玩的!</h3>
+            <p className="desc">坏新闻总比没新闻要好！前段时间, 经过一波朋友圈的疯狂转发,大家都在讨论"红芯浏览器", 不管是不是套路, 反正是出名了...最后的结论就是, ...</p>
+          </ListInfo>
+          <img className="list-pic" src ="https://upload-images.jianshu.io/upload_images/3203841-bd64bf1ec52425b9.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240" />
+        </ListItem>
+        <ListItem>
+          <ListInfo>
+            <h3 className="title">Chrome吃内存的能力可不是说着玩的!</h3>
+            <p className="desc">坏新闻总比没新闻要好！前段时间, 经过一波朋友圈的疯狂转发,大家都在讨论"红芯浏览器", 不管是不是套路, 反正是出名了...最后的结论就是, ...</p>
+          </ListInfo>
+          <img className="list-pic" src ="https://upload-images.jianshu.io/upload_images/3203841-bd64bf1ec52425b9.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240" />
+        </ListItem>
+        <ListItem>
+          <ListInfo>
+            <h3 className="title">Chrome吃内存的能力可不是说着玩的!</h3>
+            <p className="desc">坏新闻总比没新闻要好！前段时间, 经过一波朋友圈的疯狂转发,大家都在讨论"红芯浏览器", 不管是不是套路, 反正是出名了...最后的结论就是, ...</p>
+          </ListInfo>
+          <img className="list-pic" src ="https://upload-images.jianshu.io/upload_images/3203841-bd64bf1ec52425b9.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240" />
+        </ListItem>
+        <ListItem>
+          <ListInfo>
+            <h3 className="title">Chrome吃内存的能力可不是说着玩的!</h3>
+            <p className="desc">坏新闻总比没新闻要好！前段时间, 经过一波朋友圈的疯狂转发,大家都在讨论"红芯浏览器", 不管是不是套路, 反正是出名了...最后的结论就是, ...</p>
+          </ListInfo>
+          <img className="list-pic" src ="https://upload-images.jianshu.io/upload_images/3203841-bd64bf1ec52425b9.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240" />
+        </ListItem>
+      </ListWrapper>
+
     )
   }
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -4,7 +4,6 @@ export const HomeWrapper = styled.div`
   display: flex;
   width: 960px;
   margin: 0 auto;
-  border: 1px solid;
 `
 
 export const HomeLeft = styled.div`
@@ -14,6 +13,7 @@ export const HomeLeft = styled.div`
     .banner-img {
       width: 625px;
       height: 270px;
+      border-radius: 10px;
     }
 `
 
@@ -24,10 +24,10 @@ export const HomeRight = styled.div`
 `
 
 export const TopicWrapper = styled.div`
-    padding: 20px 0 10px 0;
+    padding: 20px 0 10px ;
     display: flex;
     flex-wrap: wrap;
-    margin-left: -18px;
+    border-bottom: 1px solid #f0f0f0;
 `
 
 export const TopicItem = styled.div`
@@ -35,10 +35,10 @@ export const TopicItem = styled.div`
     line-height: 32px;
     font-size: 14px;
     background: #f7f7f7;
-    border: 1px solid #dcdcdc;
+    border: 1px solid #f0f0f0;
     border-radius: 4px;
     padding-right: 10px;
-    margin-left: 18px;
+    margin-right: 18px;
     margin-bottom: 15px;
     .topic-pic {
       width: 32px;
@@ -46,3 +46,33 @@ export const TopicItem = styled.div`
       margin-right: 10px;
     }
 `
+
+export const ListWrapper = styled.div`
+
+`
+
+export const ListItem = styled.div`
+    display: flex;
+    padding: 20px 0;
+    border-bottom: 1px solid #f0f0f0;
+    .list-pic {
+      width: 125px;
+      height: 100px;
+      margin-left: 40px;
+      border: 1px solid #f0f0f0;
+      border-radius: 5px;
+    }
+    .title {
+      line-height: 27px;
+      font-size: 18px;
+      font-weight: bold;
+      color: #333;
+    }
+    .desc {
+      line-height: 24px;
+      font-size: 13px;
+      color: #999;
+    }
+`
+
+export const ListInfo = styled.div``
