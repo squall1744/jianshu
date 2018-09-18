383ba9312fadc8195d2304d6ac9d63f62c114f0f
squall1744 <squall1744@163.com>
2018-09-18 20:50:57 +0800

    add react-router-dom

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
+import {TopicWrapper} from '../style'
 
 class Topic extends Component {
   render() {
     return (
-      <div>Topic</div>
+      <TopicWrapper></TopicWrapper>
     )
   }
 }
