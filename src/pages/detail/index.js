10904f8d73ec1888fb93d327e6262491bd7314b4
squall1744 <squall1744@163.com>
2018-09-26 20:29:55 +0800

    add detail page

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react'
+import {DetailWrapper, Header, Content} from './style'
 
 class Detail extends Component {
   render() {
     return (
-      <div>Detail</div>
+      <DetailWrapper>
+        <Header>为什么腾讯不会成为一家伟大的公司？程序员有话要说</Header>
+        <Content>
+          <img src="https://upload-images.jianshu.io/upload_images/13133049-5b1a1734918a0d10.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/582/format/webp" alt="" />
+        </Content>
+      </DetailWrapper>
     )
   }
 }
